0474fc28526ebd7dfd8b4f1d6a2d7a2233c0799c
Vidit Vats <46197417+vidit-vats@users.noreply.github.com>
2023-04-23 17:00:44 +0530

    Add files via upload

---
+++ q23.sh
@@ -0,0 +1,33 @@
+# for n=3, answer should come as 2.5
+factorial()
+{
+ i=1
+	
+  	f=1
+
+	for ((i=1;i<=arg;i++))
+	do
+		f=$(echo "scale=2;$f*$i" | bc)
+	done
+echo $f
+
+}
+
+
+#res=$(factorial $no)
+
+echo -n "Enter the range upto which you want sum: "
+read n 
+
+fin_res=0
+
+for ((arg=1;arg<=n;arg++))
+do
+	fun_res=$(factorial $arg)
+	bc_res=$(echo "scale=2;$arg/$fun_res" | bc)
+	fin_res=$(echo "$bc_res + $fin_res" | bc)
+	#echo "Final Result: $fin_res"
+done
+
+echo
+echo "Sum of the desired series: $fin_res"
---
+++ q26.sh
@@ -0,0 +1,10 @@
+echo -n "Enter your age: "
+read age 
+
+if [[ $age -ge 18 ]] 
+then
+	echo "Eligible to vote"
+else
+	echo "Not eligible to vote"
+fi
+
